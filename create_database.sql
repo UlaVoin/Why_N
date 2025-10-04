bd551030b19aa97d6e3dd2db693616978ac38017
ENOTIK1307 <ttimur1307@gmail.com>
2025-10-04 12:07:12 +0500

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -8,7 +8,12 @@ USE users_db;
 -- Таблица пользователей
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+<<<<<<< Updated upstream
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+=======
+    username VARCHAR() UNIQUE NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+>>>>>>> Stashed changes
