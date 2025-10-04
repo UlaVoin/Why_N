c416bbd98bb924d90164a5582aaf847a5f0332b8
ENOTIK1307 <ttimur1307@gmail.com>
2025-10-04 10:59:30 +0500

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS users_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE users_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
